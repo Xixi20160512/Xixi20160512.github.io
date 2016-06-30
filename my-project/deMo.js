3b973aa055a62f9e39563e264fb1f61253d59115
Xixi20160512 <1174308406@qq.com>
2016-06-30 21:31:01 +0800

    添加fullpage单页面展示

---
+++ my-project/deMo.js
@@ -0,0 +1,14 @@
+document.write("这是一个deMo");
+window.onlode(fun())
+function fun() {
+	alert("不知道可不可以成功啊");
+}
+
+if (true) {}Tersis co ltd
+Address: office 402, Svetlanskaya street 167, Vladivostok city,
+Account: 40702840211022003002
+Beneficiary’s bank: JSC VTB Bank (Khabarovsk branch),  
+Russia, Khabarovsk, Moskovskaya str, 7,
+S.W.I.F.T.    VTBRRUM2KHA             
+BIC  040813727
+Correspondent account: 30101810400000000727
