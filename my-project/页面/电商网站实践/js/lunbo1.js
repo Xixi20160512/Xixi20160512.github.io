b271ca5a081fac79473fa1dfb4d33096f9540217
Xixi20160512 <1174308406@qq.com>
2016-07-16 02:15:45 +0800

    debug

--- my-project/页面/电商网站实践/js/lunbo1.js
+++ my-project/页面/电商网站实践/js/lunbo1.js
@@ -68,10 +68,11 @@
 		li[now].className = 'on';
 	}
         function css(obj, attr){
-            if(obj.currentStyle){
-                return obj.currentStyle[attr];
-            } else {
-                return getComputedStyle(obj, false)[attr];
-            }
-        }
+		 if(obj.currentStyle){
+		 	 return obj.currentStyle[attr];
+			 }
+		 else{
+		  	return getComputedStyle(obj, false)[attr];
+		 }
+		}
 })()
