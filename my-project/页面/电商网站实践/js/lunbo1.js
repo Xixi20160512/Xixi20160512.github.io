0b30f7af2dc009ca3e85d8a704e852be82809f4e
Xixi20160512 <1174308406@qq.com>
2016-07-19 14:06:20 +0800

    Update lunbo1.js

--- my-project/页面/电商网站实践/js/lunbo1.js
+++ my-project/页面/电商网站实践/js/lunbo1.js
@@ -50,8 +50,7 @@ window.onload = define('lunbo1',function (require) {
 			}
 			li[now].className = 'on';
 		}
-	}
-//轮播2
+	//轮播2
 	var box1 = document.getElementById('lunbo2');
 	var imgs = box1.getElementsByTagName('img');
 	var nav_box = box1.getElementsByTagName('div')[0];
@@ -81,5 +80,7 @@ window.onload = define('lunbo1',function (require) {
 		}
 		nav2s[now].className += ' on2';
 	}
+	}
+
 ) 
 
